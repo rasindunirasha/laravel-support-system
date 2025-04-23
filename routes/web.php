91d5c13226594de57caaf75cad76617d95523151
rasindunirashagithub <rasindunirasha7@gmail.com>
2025-04-23 15:03:05 +0530

    Routes signup pages

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@ Route::group(['namespace' => 'App\Http\Controllers'], function() { // GROUP 1
     Route::get('/login', 'LoginController@login')->name('login');
     Route::post('/login', 'LoginController@authenticate')->name('authenticate');
 
+    Route::get('/signup', 'RegisterController@showRegistrationForm')->name('signup');
+    Route::post('/signup', 'RegisterController@register')->name('register');
+
     Route::get('/tickets/search', 'TicketController@search')->name('tickets.search');
     Route::get('/tickets/create', 'TicketController@create')->name('tickets.create');
     Route::post('/tickets', 'TicketController@store')->name('tickets.store');
