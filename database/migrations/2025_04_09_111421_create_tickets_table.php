55076c137fb8210460f5a7450f336ed98ec2d639
rasindunirashagithub <rasindunirasha7@gmail.com>
2025-04-25 10:54:56 +0530

    some change

--- database/migrations/2025_04_09_111421_create_tickets_table.php
+++ database/migrations/2025_04_09_111421_create_tickets_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+
+            
             $table->string('customer_name');
             $table->string('email');
             $table->string('phone')->nullable();
